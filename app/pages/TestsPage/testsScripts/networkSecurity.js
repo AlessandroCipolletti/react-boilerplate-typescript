da74572478b5a182c15365b31d8c17fde81295ac
Alessandro Cipolletti <cipolletti.alessandro@gmail.com>
2020-04-07 15:52:46 +0200

    Update networkSecurity.js

--- app/pages/TestsPage/testsScripts/networkSecurity.js
+++ app/pages/TestsPage/testsScripts/networkSecurity.js
@@ -1,18 +1,21 @@
-const getFetchPromise = async(url) => {
+const getFetchPromise = async([key, url]) => {
   try {
     const response = await fetch(url)
     if (response.ok) {
-      return [url, {
+      return [key, {
+        url,
         status: 'success',
         err: false,
       }]
     }
-    return [url, {
+    return [key, {
+      url,
       status: 'error',
       err: `HTTP Status: ${response.status}`,
     }]
   } catch(err) {
-    return [url, {
+    return [key, {
+      url,
       status: 'error',
       err,
     }]
@@ -22,16 +25,18 @@ const getFetchPromise = async(url) => {
 export default async function() {
   await ((ms) => new Promise(r => setTimeout(r, ms)))(Math.random() * 1000 + 2000)
 
-  const urlsToTest = [
-    'https://s3-eu-west-1.amazonaws.com/pitchy-v3/assets/icons/icone_atelier.png',
-    'https://pitchy-v3.s3-eu-west-1.amazonaws.com/assets/icons/icone_atelier.png',
-    'https://cdn.mxpnl.com/',
-    'https://api.mixpanel.com/',
-    'https://pitchy-support.zendesk.com/',
-  ]
+  const urlsToTest = {
+    'app.pitchy.fr': 'https://app.pitchy.fr/login',
+    'prod_atelier_js_boundle': 'https://s3-eu-west-1.amazonaws.com/pitchy-v3/assets/site/js/atelier.c182e632051168260dab.min.js',
+    's3-eu-west-1.amazonaws.com/pitchy-v3': 'https://s3-eu-west-1.amazonaws.com/pitchy-v3/assets/icons/icone_atelier.png',
+    'pitchy-v3.s3-eu-west-1.amazonaws.com': 'https://pitchy-v3.s3-eu-west-1.amazonaws.com/assets/icons/icone_atelier.png',
+    'cdn.mxpnl.com': 'https://cdn.mxpnl.com/',
+    'api.mixpanel.com': 'https://api.mixpanel.com/',
+    'pitchy-support.zendesk.com': 'https://pitchy-support.zendesk.com/',
+  }
 
   const results = await Promise.all(
-    urlsToTest.map(getFetchPromise)
+    Object.entries(urlsToTest).map(getFetchPromise)
   )
 
   return Object.fromEntries(results)
